c29fced89b61e5a06debaf78df8e9159888e8375
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2022-06-16 11:48:00 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,39 +1,145 @@
-let json = "noticias.json";
-let XHR = new XMLHttpRequest();
-XHR.open( "GET", json );
 
-XHR.responsetype = "json";
-XHR.send();
+let requisicao_URL = "https://www.luizpicolo.com.br/api.json";
+let requisicao = new XMLHttpRequest();
+requisicao.open( "GET", requisicao_URL );
+requisicao.responseType = "json";
+requisicao.send();  
 
-class Noticia{
-  constructor( title , img , data , resumo , texto ){
-    this.title = title;
-    this.img = img;
-    this.data = data; 
-    this.resumo = resumo;
-    this.texto = texto;
+
+class Mensagem_Error extends Error{
+  constructor( message ){
+    super( message )
+    this.name =  "Error Message: ";
   }
   
-  mostrarNoticia(){
+  get_Message(){
     
-    return `${this.title}\n\n${this.img}\n\n${this.data}\n\n${this.resumo}\n\n${this.texto}`
+        return `<div id="container-noticias">
+                <h2>${this.name}</h2>
+                <p>${this.message}</p>
+                </div>`;
     
   }
 }
 
-XHR.onload = function(){
-var resposta = XHR.response;
+class Noticia_Destaque{
+ 
+  set_Atributos_Destaque( imagem , data , titulo , autor , resumo , url ){
+    this.imagem = imagem;
+    this.data = data;
+    this.titulo = titulo;
+    this.autor = autor; 
+    this.resumo = resumo;
+    this.url = url;
+   }
+   
+  set_Noticia_Destaque(){
+     
+ //   if( this.imagem == undefined || this.title == undefined || this.data == undefined || this.autor == undefined || this.resumo == undefined || this.url == undefined ){
+       
+//    throw new Mensagem_Error( "Alguns dados na API json podem estar indefinidos" );
+       
+//  }else{
+       
+ return `<div id="container-noticia_destaque">
+          <center><img src="${this.imagem}" id="imagem_destaque"></center>
+          <h3>${this.titulo}</h3>
+          <h5>Publicado: ${this.data}</h1>
+          <h5>Autor: ${this.autor}</h1>
+          <p>${this.resumo}</p>
+          <center><a href="${this.url}">Clique aqui para ver a noticia completa</a></center>
+         </div>`;
+ //  }   
+}
 
-let noticias = JSON.parse(resposta);
+ mostrar_Noticia_Destaque(){
+   
+ // try{
+     let id = document.getElementById("noticias");
+     return id.insertAdjacentHTML( 'afterbegin' , this.set_Noticia_Destaque() );
+    
+ //  }catch(error){
+   
+ //    let id = document.getElementById("noticias");
+ //    return id.insertAdjacentHTML( 'afterbegin' , error.get_Message() );
+    
+ //   }
+  }
+}
 
-let titulo1 = noticias[1].titulo;
-let imagem1 = noticias[1].imagem_destaque;
-let data1 = noticias[1].data_publicacao;
-let resumo1 = noticias[1].resumo;
-let texto1 = noticias[1].texto;
 
-let noticia = new Noticia( titulo1 , imagem1 , data1 , resumo1 , texto1 );
+
+//
+
+
+class Noticias{
+    
+set_Atributos( titulo , data , autor , resumo , url ){
   
-console.log(noticia.mostrarNoticia());
+  this.titulo = titulo;
+  this.name = name;
+  this.data = data;
+  this.autor = autor;
+  this.resumo = resumo;
+  this.url = url;
   
+}
+
+setNoticias(){
+  
+  /**if( this.title == undefined || this.data == undefined || this.autor == undefined || this.resumo == undefined || this.url == undefined ){
+    
+    throw new Mensagem_Error( "Alguns dados podem estar indefinidos");
+    
+  }else{
+**/    
+  return `<div id="container-noticias">
+          <h3>${this.titulo}</h3>
+          <h5>Publicado: ${this.data}</h1>
+          <h5>Autor: ${this.autor}</h1>
+          <p>${this.resumo}</p>
+          <center><a href="${this.url}">Clique aqui para ver a noticia completa</a></center>
+          </div>`;
+  //}
+}
+
+ mostrar_Noticias(){
+  
+//try{
+  
+     let id = document.getElementById("container-noticia_destaque");
+     return id.insertAdjacentHTML( 'afterend' , this.setNoticias() );
+  
+//   }catch(error){
+    
+//     let id = document.getElementById("noticias");
+ //    return id.insertAdjacentHTML( 'beforeend' , error.get_Message() );
+  
+//    }
+  }
+}
+
+
+//
+
+requisicao.onload = function(){
+var noticias = requisicao.response;
+
+let noticia_destaque = new Noticia_Destaque();
+let all_noticias = new Noticias();
+
+  for(let i = 0 ; i <= noticias.articles.length ; i++){
+     
+      if( i == 0 ){
+        noticia_destaque.set_Atributos_Destaque( noticias.articles[i].urlToImage, noticias.articles[i].publishedAt , noticias.articles[i].title , noticias.articles[i].author , noticias.articles[i].description , noticias.articles[i].url );
+        noticia_destaque.mostrar_Noticia_Destaque();
+ }
+      
+      if( i >= 1 ){
+        all_noticias.set_Atributos( noticias.articles[i].title , noticias.articles[i].publishedAt , noticias.articles[i].author , noticias.articles[i].description , noticias.articles[i].url );
+        all_noticias.mostrar_Noticias();
+   }
+}
+  
+     
 }
